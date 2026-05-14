06034be695b66041e86ca3cd7ec9f06a78e3ffb3
Robert West <robert.trey.west@gmail.com>
2026-05-14 00:29:01 -0400

    Completed Exercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { COLORS, QUERIES } from '../../constants';
 
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
@@ -15,12 +16,11 @@ const SpecialtyStoryGrid = () => {
           cornerLink={{
             href: '/markets',
             content: 'Visit Markets data »',
-          }}
-        >
+          }}>
           Markets
         </SectionTitle>
         <MarketCards>
-          {MARKET_DATA.map((data) => (
+          {MARKET_DATA.map(data => (
             <MarketCard key={data.tickerSymbol} {...data} />
           ))}
         </MarketCards>
@@ -30,15 +30,16 @@ const SpecialtyStoryGrid = () => {
           cornerLink={{
             href: '/sports',
             content: 'Visit Sports page »',
-          }}
-        >
+          }}>
           Sports
         </SectionTitle>
-        <SportsStories>
-          {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
-          ))}
-        </SportsStories>
+        <ScrollContainer>
+          <SportsStories>
+            {SPORTS_STORIES.map(data => (
+              <MiniStory key={data.id} {...data} />
+            ))}
+          </SportsStories>
+        </ScrollContainer>
       </SportsSection>
     </Wrapper>
   );
@@ -47,14 +48,49 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: 100%;
+    gap: 64px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr minmax(0, 1fr);
+    gap: 0;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid ${COLORS.gray[300]};
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(183px, 1fr));
+  gap: 16px;
+`;
 
 const SportsSection = styled.section``;
 
-const SportsStories = styled.div``;
+const ScrollContainer = styled.div`
+  overflow: auto;
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(183px, 1fr));
+  gap: 16px;
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: revert;
+    grid-auto-columns: 220px;
+    grid-template-rows: 1fr;
+    grid-auto-flow: column;
+  }
+`;
 
 export default SpecialtyStoryGrid;
